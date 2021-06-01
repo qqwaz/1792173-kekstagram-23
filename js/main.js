2c4e8e8ffd91f64edaffe23485bf2eed8dc87ea0
bpd <bpd@mail.ru>
2021-06-01 12:51:37 +0300

    corrections after review

--- js/main.js
+++ js/main.js
@@ -3,15 +3,13 @@ const COMMENT_MAX_LENGTH = 140;
 const TAG_PATTERN = /^#[\p{L}\p{N}]{1,19}$/u;
 
 
-const getIntFromRange = (min, max) => {
-  [min, max] = (min <= max)
-    ? [Math.ceil(min), Math.floor(max)]
-    : [Math.ceil(max), Math.floor(min)];
+const getIntFromRange = (min, max) =>
+  min >= 0 && Math.ceil(min) <= Math.floor(max)
+    ? Math.floor(Math.random() * (max - min + 1) + min)
+    : -1;
 
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
-const isCommentValid = (comment) => comment === '' || comment.length <= COMMENT_MAX_LENGTH;
+const isCommentValid = (comment) =>
+  comment.length <= COMMENT_MAX_LENGTH;
 
 const areTagsValid = (tags) => {
   if (tags === '') {
@@ -25,6 +23,6 @@ const areTagsValid = (tags) => {
       index === arr.indexOf(tag) && TAG_PATTERN.test(tag));
 };
 
-getIntFromRange();
-isCommentValid();
-areTagsValid();
+getIntFromRange(0.005, 2);
+isCommentValid('');
+areTagsValid('');
